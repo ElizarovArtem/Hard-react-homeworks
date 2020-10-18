490590d6a8b87f8ee49797e5a2f96180d0409bf6
ElizarovArtem <elizarovskiyklach@yandex.ru>
2020-10-18 15:38:45 +0300

    doing 5 homework

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
         <div>
             // add NavLinks
-
+            <NavLink to={"/junior"}>JUNIOR</NavLink>
+            <NavLink to={"/pre-junior"}>PRE-JUNIOR</NavLink>
         </div>
     );
 }
--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 
 function Error404() {
+    const numberStyle = {
+        marginLeft: "800px",
+        fontSize: "10em"
+    }
+    const notFoundStyle = {
+        marginLeft: "400px",
+        fontSize: "10em",
+        color: "red"
+    }
+    const catStyle = {
+        marginLeft: "500px",
+        fontSize: "10em",
+        color: "green"
+    }
+    const pageStyle = {
+
+        backgroundColor: "purple"
+    }
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div style={pageStyle}>
+            <div style={numberStyle}>404</div>
+            <div style={notFoundStyle}>Page not found!</div>
+            <div style={catStyle}>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
         </div>
     );
 }
