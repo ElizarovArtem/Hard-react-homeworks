c5d09024cf462ad0666b992c81b5a1672274b062
ElizarovArtem <elizarovskiyklach@yandex.ru>
2020-10-22 20:48:42 +0300

    done 6 homework and correct 5 homework

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -5,14 +5,13 @@ import {HashRouter} from "react-router-dom";
 import s from "./../../p1-main/m1-ui/u1-app/App.module.css"
 
 export function HW5() {
-    let[filter, setFilter] = useState("pre-junior")
 
     return (
         <div className={s.h5}>
             {/*в gh-pages лучше работает HashRouter*/}
             <HashRouter>
 
-            <Header filter={filter} setFilter={setFilter}/>
+            <Header/>
 
             <Routes/>
 
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -2,21 +2,15 @@ import React, {useState} from "react";
 import {NavLink} from "react-router-dom";
 import s from "./../../p1-main/m1-ui/u1-app/App.module.css"
 
-type HeaderPropsType = {
-    filter: string
-    setFilter: (value: string) => void
-}
 
-function Header(props: HeaderPropsType) {
-    let [collapse, setCollapse] = useState(true)
+function Header() {
 
     return (
         <div className={s.sss}>
-            <div className={s.menu} onClick={() => setCollapse(!collapse)}>Menu</div>
-             <div className={collapse ? s.links : s.linksOpen}>
-                <div><NavLink className={props.filter === "junior" ? s.active : ""} to={"/junior"} onClick={() => props.setFilter("junior")}>JUNIOR</NavLink></div>
-                <div><NavLink className={props.filter === "juniorPlus" ? s.active : ""} to={"/juniorPlus"} onClick={() => props.setFilter("juniorPlus")}>JUNIOR-PLUS</NavLink></div>
-                <div><NavLink className={props.filter === "pre-junior" ? s.active : ""} to={"/pre-junior"} onClick={() => props.setFilter("pre-junior")} >PRE-JUNIOR</NavLink></div>
+             <div className={s.links}>
+                <div><NavLink activeClassName={s.active}  to={"/junior"} >JUNIOR</NavLink></div>
+                <div><NavLink activeClassName={s.active}  to={"/juniorPlus"} >JUNIOR-PLUS</NavLink></div>
+                <div><NavLink activeClassName={s.active}  to={"/pre-junior"} >PRE-JUNIOR</NavLink></div>
             </div>
         </div>
     );
